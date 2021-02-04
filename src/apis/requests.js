56c0770bf39bf0199dfb3ccb871a2d4c18fbfb0d
Melvin Gaye <melvind.gaye@gmail.com>
2021-02-04 17:26:17 -0500

    switched the contact and suggestions endpoints

--- src/apis/requests.js
+++ src/apis/requests.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 function sendBookSuggestion(mesg){
-    axios.post('/.netlify/functions/send-contact-info',mesg)
+    axios.post('/.netlify/functions/send-book-suggestion',mesg)
     .then( res =>{console.log(res)})
     .catch( err =>{console.log(err)})
 }
 
 function sendContactInfo(mesg){
     debugger
-    axios.post('/.netlify/functions/send-book-suggestion',mesg)
+    axios.post('/.netlify/functions/send-contact-info',mesg)
     .then( res =>{console.log(res)})
     .catch( err =>{console.log(err)})
 }
