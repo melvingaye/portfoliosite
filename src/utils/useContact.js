a7231a69c6793fc11e312b4089bc294af896f4df
Melvin Gaye <melvind.gaye@gmail.com>
2021-02-07 23:53:50 -0500

    completed useContact hook

--- src/utils/useContact.js
+++ src/utils/useContact.js
@@ -1,7 +1,20 @@
 
-function useContact(){
-    const [name, setName] = useState('')
-    const [phoneNumber, setPhoneNumber] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
+import {sendContactInfo} from '../apis/requests'
+
+export function useContact(){
+    const [values, setValues] = useState({name: '', phone: '', email: '', content: ''})
+
+    const handleChange = e => {
+       const {name, value} = e.target
+       setValues({...values, [name]: value})
+
+
+    }
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        sendContactInfo(values)
+    }
+
+    return {handleChange, handleSubmit, values}
 }
