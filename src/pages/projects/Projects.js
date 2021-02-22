8823e0a2e2d4f1c62455991935bc2e4466dd922c
Melvin Gaye <melvind.gaye@gmail.com>
2021-02-22 11:06:29 -0500

    changed profile display to only show if user is logged in

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import {linkedInAuth} from '../../apis/requests'
+import React, { useEffect, useState } from 'react';
+import {linkedInAuth} from '../../apis/requests';
 
 function Projects(){
+    const [auth, setAuth] = useState(false);
     const [profile, setProfile] = useState({
         firstname: '',
         lastname: '',
         email: '',
         avatar: ''
-    })
+    });
 
     const handleClick = async () => {
         const response = await linkedInAuth();
         // open the linkedin-social auth in the current window
         window.location.href = response.data;
-    }
+    };
   
     useEffect(()=>{
         document.title='Software Engineer Resume Projects | Melvin Gaye'
@@ -21,21 +22,28 @@ function Projects(){
         const userstr = localStorage.getItem('user');
         if(userstr){
             const profile = JSON.parse(userstr);
-            setProfile(profile)
+            setProfile(profile);
+            setAuth(true);
         }
 
     },[]);
 
-    return(
+    return auth ? (
         <div className="projects">
             <div style={{display: 'flex', flexDirection: 'column'}}>
-            <h1>Projects</h1>
-            <button className="btn" onClick={handleClick}>Login with LinkedIn</button>
-            <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}><h4>First: </h4><h6>{profile?.firstname}</h6></div>
-            <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}><h4>Last: </h4><h6>{profile?.lastname}</h6></div>
-            <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}><h4>Email: </h4><h6>{profile?.email}</h6></div>
-            <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}><img src={profile?.avatar} alt="linkedin user account"/></div>
+                <h1>User Dashboard</h1>
+                <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}><h4>First: </h4><h6>{profile?.firstname}</h6></div>
+                <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}><h4>Last: </h4><h6>{profile?.lastname}</h6></div>
+                <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}><h4>Email: </h4><h6>{profile?.email}</h6></div>
+                <div style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}><img src={profile?.avatar} alt="linkedin user account"/></div>
             </div>
+        </div>
+    ) : (
+        <div className="projects">
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+        <h1>Sign In</h1>
+        <button className="btn" onClick={handleClick}>Login with LinkedIn</button>
+        </div>
         </div>
     )
 }
